850fddd50d037251afc9b573e51014cfe19be79b
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-01-01 20:46:36 +0100

    setup font configuration for EazyKash app.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client"
+import {Sora, Inter} from 'next/font/google'
+
+/*FONT CONFIGURATIONS
+
+Headings: Inter [Semibold]
+Subheadings: Inter [Medium]
+Body Text: Sora
+
+*/
+
+const sora = Sora({
+  weight: ["400"],
+  subsets: ["latin"],
+  style: "normal",
+});
+
+const interMedium = Inter({
+  weight: ["600"],
+  subsets: ["latin"],
+  style: "normal",
+});
+
+const interSemiBold = Inter({
+  weight: ["700"],
+  subsets: ["latin"],
+  style: "normal",
+});
+
 
 export default function Home() {
   return (
     <>
-    <h1>Welcome To EazyKash</h1>
+    <h1 className={`${interSemiBold.className} text-2xl font-medium`}>Welcome To EazyKash</h1>
     </>
   );
 }
